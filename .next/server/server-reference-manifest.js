21925a9020bc7cbccb6861d07b8b7832dca3061b
Christian Nickel <christiannickel95@hotmail.com>
2025-12-02 20:39:45 +0100

    correcting build error

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9Uf4Mt3drlVC87ZM9S/wwQP8PxqAv3qXuB++rVpV/gs=\"\n}"
--- app/produkte/[category]/[slug]/page.tsx
+++ app/produkte/[category]/[slug]/page.tsx
@@ -112,25 +112,27 @@ export default function ProductPage({ params }: ProductPageProps): JSX.Element {
               </Card>
 
               {/* Product Benefits */}
-              <Card>
-                <CardHeader>
-                  <CardTitle>Vorteile & Features</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <ul className="space-y-2">
-                    {product.benefits.map((benefit, index) => (
-                      <li key={index} className="flex items-start">
-                        <div className="w-5 h-5 bg-green-500 rounded-full flex items-center justify-center mr-3 mt-0.5 flex-shrink-0">
-                          <svg className="w-3 h-3 text-white" fill="currentColor" viewBox="0 0 20 20">
-                            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                          </svg>
-                        </div>
-                        <span className="text-gray-700">{benefit}</span>
-                      </li>
-                    ))}
-                  </ul>
-                </CardContent>
-              </Card>
+              {product.benefits && product.benefits.length > 0 && (
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Vorteile & Features</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <ul className="space-y-2">
+                      {product.benefits.map((benefit, index) => (
+                        <li key={index} className="flex items-start">
+                          <div className="w-5 h-5 bg-green-500 rounded-full flex items-center justify-center mr-3 mt-0.5 flex-shrink-0">
+                            <svg className="w-3 h-3 text-white" fill="currentColor" viewBox="0 0 20 20">
+                              <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                            </svg>
+                          </div>
+                          <span className="text-gray-700">{benefit}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </CardContent>
+                </Card>
+              )}
 
               {/* Product Variants */}
               <Card>
